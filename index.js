bfac3941e73cd3decd05478789a8ef777a61707b
paparna1998 <107532773+paparna1998@users.noreply.github.com>
2022-09-30 12:02:42 +0530

    feat: create web app

---
+++ index.js
@@ -0,0 +1,45 @@
+var readlineSync = require('readline-sync');
+let userName = readlineSync.question(`What's your name?`)
+console.log('Welcome ' + userName + '. How well do you know Amulya ?' + ` Let's take a quiz, shall we ?`)
+
+let counter = 0
+
+let quiz = (question, answer) => {
+  let userAnswer = readlineSync.question(question)
+
+  if (userAnswer === answer) {
+    console.log('Correct!')
+    counter++
+
+  } else {
+    console.log('Oops, Wrong Answer *.*')
+  }
+
+  console.log('Current Score : ', counter)
+  console.log('----------------')
+
+
+}
+
+
+let array = [{
+  question: 'Where do i live ?',
+  answer: 'mumbai'
+}, {
+  question: `What's the name of the game i play the most ?`,
+  answer: 'chess'
+}, {
+  question: `which is my favorite dessert?`,
+  answer: 'kala jamun'
+}, {
+  question: `The month name of my birthday is ?`,
+  answer: 'january'
+}, {
+  question: `What do i aspire to become ?`,
+  answer: 'full stack web developer'
+}]
+
+array.forEach((value) => {
+  quiz(value.question, value.answer)
+
+})
